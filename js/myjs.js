fd312bd18801057c07f9484d33dfcbbacab16de8
Vincent Wang <v_wang@outlook.com>
2017-01-22 16:43:19 -0500

    Add light box effect

--- js/myjs.js
+++ js/myjs.js
@@ -33,3 +33,7 @@ $(document).ready(function() {
     }
   });
 });
+$(document).on('click', '[data-toggle="lightbox"]', function(event) {
+    event.preventDefault();
+    $(this).ekkoLightbox();
+});
